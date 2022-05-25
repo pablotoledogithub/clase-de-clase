b8e97c5b1e040d7bd7f4dca3c4bfbfb5cc619620
pablotoledogithub <98492120+pablotoledogithub@users.noreply.github.com>
2022-05-24 23:14:09 -0300

    primero commit

---
+++ index.ts
@@ -0,0 +1,71 @@
+// crear las clases Edificio, Piso y Departamento aquí
+class Edificio {
+  pisos: Piso[];
+  constructor(pisos: Piso[]) {
+    this.pisos = pisos;
+  }
+  addDepartamentoToPiso(nombreDePiso: string, departamento: Departamento) {
+    const pisoEncontrado = this.pisos.find((p) => p.nombrePiso == nombreDePiso);
+    return pisoEncontrado.pushDepartamento(departamento);
+  }
+  getDepartamentosByPiso(nombreDePiso: string): Departamento[] {
+    const pisoEncontrado = this.pisos.find((p) => p.nombrePiso == nombreDePiso);
+    return pisoEncontrado.getDepartamentos();
+  }
+}
+class Piso {
+  nombrePiso: string;
+  deptos: Departamento[];
+  constructor(nombrePiso) {
+    this.nombrePiso = nombrePiso;
+    this.deptos = [];
+  }
+  pushDepartamento(depto: Departamento) {
+    return this.deptos.push(depto);
+  }
+  getDepartamentos(): Departamento[] {
+    return this.deptos;
+  }
+}
+
+class Departamento {
+  nombre: string;
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+  getName() {
+    return this.nombre;
+  }
+}
+
+// no modificar este test
+function testClaseEdificio() {
+  const unPiso = new Piso("planta baja");
+  const otroPiso = new Piso("primer piso");
+  const unEdificio = new Edificio([unPiso, otroPiso]);
+  const deptoUno = new Departamento("depto uno");
+  const deptoDos = new Departamento("depto dos");
+  const deptoTres = new Departamento("depto tres");
+  unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
+
+  const deptos = unEdificio.getDepartamentosByPiso("planta baja");
+  const deptosEmpty = unEdificio.getDepartamentosByPiso("primer piso");
+
+  if (
+    Array.isArray(deptosEmpty) &&
+    deptosEmpty.length == 0 &&
+    deptos.length == 3 &&
+    deptos[2].getName() == "depto tres"
+  ) {
+    console.log("testClaseBandaApartment passed");
+  } else {
+    throw "testClaseBandaApartment not passed";
+  }
+}
+
+function main() {
+  testClaseEdificio();
+}
+main();
